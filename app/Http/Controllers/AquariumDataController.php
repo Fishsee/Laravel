4dc87a3dbdf49d11cc07e36f44f077f7bd151ee8
Aaron de Bruin <aaron.de.bruin@student.nhlstenden.com>
2024-05-17 09:54:28 +0200

    added troebelheid routes, and functions

--- app/Http/Controllers/AquariumDataController.php
+++ app/Http/Controllers/AquariumDataController.php
@@ -43,4 +43,45 @@ class AquariumDataController extends Controller
             'data' => $latestPH
         ]);
     }
+
+    public function getAllTurbidity(Request $request)
+    {
+        $items = AquariumData::where('Troebelheid', '>', 0)->get(['Troebelheid']);
+
+        if ($items->isEmpty()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No turbidity values found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $items
+        ]);
+    }
+
+
+    public function getLatestTurbidity(Request $request)
+    {
+        $latestTurbidity = AquariumData::where('Troebelheid', '>', 0)
+            ->orderBy('created_at', 'desc')
+            ->first(['Troebelheid']);
+
+        // Check if no value was found and return an error response
+        if (!$latestTurbidity) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No turbidity value found'
+            ], 404);
+        }
+
+        // If a value was found, return it in the response
+        return response()->json([
+            'status' => 'success',
+            'data' => $latestTurbidity
+        ]);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Routes for PH data
 Route::get('/all-PH', [AquariumDataController::class, 'getAllPH']);
 Route::get('/last-PH', [AquariumDataController::class, 'getLatestPH']);
 
+// Routes for troebelheid data
+Route::get('/all-troebelheid', [AquariumDataController::class, 'getAllTurbidity']);
+Route::get('/last-troebelheid', [AquariumDataController::class, 'getLatestTurbidity']);
 
 
 
