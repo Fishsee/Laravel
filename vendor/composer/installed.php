0e313f66b8d3c1f52cd967a7fffa0af6c708577c
DaanRijfers <daanrijfers@ziggo.nl>
2024-05-27 08:07:08 +0000

    Ik heb de .env en database.php veranderd ik heb bij het .env bestand het ip en de APPlocation naar het DNS veranderd en bij database.php het ip veranderd

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '552779836a59f8fdd249c97f037956477ff8817f',
+        'reference' => NULL,
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -380,12 +380,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '552779836a59f8fdd249c97f037956477ff8817f',
+            'reference' => NULL,
             'dev_requirement' => false,
         ),
         'laravel/pint' => array(
